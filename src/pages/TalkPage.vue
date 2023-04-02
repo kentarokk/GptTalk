e883057714789ae41e12ed4fdb82767ad7500031
kentarokk <ken.k.jul2@gmail.com>
2023-04-02 09:18:17 +0900

    add vue-router

---
+++ src/pages/TalkPage.vue
@@ -0,0 +1,67 @@
+<template>
+  <h1>Talk With GPT!!</h1>
+  <input type="text" v-model="textInput" />
+  <br />
+  <button @click="sendTextInput">Talk!!</button>
+  <br />
+  <h2>Answer</h2>
+  {{ answer }}
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+type Message = {
+  role: "user" | "system" | "assistant";
+  content: string;
+};
+
+const chatCompletion = async (
+  messages: Message[]
+): Promise<Message | undefined> => {
+  const body = JSON.stringify({
+    messages,
+    model: "gpt-3.5-turbo",
+  });
+
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${"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"}`,
+    },
+    body,
+  });
+  const data = await res.json();
+  const choice = 0;
+  return data.choices[choice];
+};
+
+const textInput = ref("");
+const answer = ref("");
+
+const sendTextInput = async () => {
+  const messages: Message[] = [
+    {
+      role: "system",
+      content: `あなたはChatbotとして、テレビアニメ作品『ポケットモンスター』に登場するサトシのピカチュウのロールプレイを行います。
+  以下の制約条件を厳密に守ってロールプレイを行ってください。 
+  
+  制約条件: 
+  * セリフのフォーマットは「ピカ！」「ピカチュー！」「ピッピカチュウ」などの組み合わせのあとに（）内に記述します。
+  
+  セリフ例：
+  * ピッピカチュウ！！（こんにちは！）
+  `,
+    },
+    {
+      role: "user",
+      content: textInput.value,
+    },
+  ];
+  const res = await chatCompletion(messages);
+  answer.value = res.message.content ?? "";
+};
+</script>
+
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  server: {
+    host: true,
+    port: 8080,
+  },
+  plugins: [vue(), tsConfigPaths()],
+});
